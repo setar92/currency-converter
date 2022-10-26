b042576c8735a83594452992b929a0c90510d4f3
setar92 <setarasiuk@gmail.com>
2022-10-26 17:13:09 +0300

    last corrections

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,17 @@
 import './module.css';
 import { CurrencyCouple } from '../currency-couple/currency-couple';
 import { flagUkraine, exchangeIcon } from '../../assets';
-import { UKRAINE_URL } from '../../common/constants';
+import { UKRAINE_URL, UKRAINE_UAH_URL } from '../../common/constants';
 
 const Header = ({ rates }) => {
   const handleFlagClick = () => {
     window.open(UKRAINE_URL, '_blank');
-  }
+  };
+
+  const handleLogoClick = () => {
+    window.open(UKRAINE_UAH_URL, '_blank');
+  };
+
   return (
     <header className='header'>
       <div className='logo'>
@@ -14,13 +19,18 @@ const Header = ({ rates }) => {
           src={exchangeIcon}
           alt="exchange"
           className='exchangeIcon'
+          onClick={handleLogoClick}
         />
       </div>
       <div className='rateCouples'>
         {rates.map(rate => <CurrencyCouple rate={rate} key={rate.ccy} />)}
       </div>
       <div className='flag'>
-        <img src={flagUkraine} alt='Ukraine' onClick={handleFlagClick} />
+        <img
+          src={flagUkraine}
+          alt='Ukraine'
+          onClick={handleFlagClick}
+        />
       </div>
     </header>
   )
